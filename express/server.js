bd7640f6252e1f7ac2efe49c920dd9a66992da89
Diky Hadna <dikyhd@gmail.com>
2020-06-05 11:27:38 +0700

    change router

--- express/server.js
+++ express/server.js
@@ -40,7 +40,7 @@ router.get('/', function (req, res) {
 
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
-app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+// app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 
 module.exports = app;
 module.exports.handler = serverless(app);
